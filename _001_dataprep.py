ed063003b7478a5542b954cefb6f6c73260dcfc9
calvinmcelvain <133781550+calvinmcelvain@users.noreply.github.com>
2024-10-13 08:14:57 -0500

    Making Trim Data function and Data Merge functions more useful
    Making it so merged data is incorporated into the Trim Data function. Previously was seperate

--- _001_dataprep.py
+++ _001_dataprep.py
@@ -5,7 +5,7 @@ Preparing data and creating test data for:
 
 import sys
 import os
-os.chdir('/Users/fogellmcmuffin/Dropbox/ai_irpd_coding/')   # Working Dir.
+os.chdir('/Users/fogellmcmuffin/Dropbox/ai_irpd_coding/data')   # Working Dir.
 
 # Modules & Packages
 sys.path.append('/Users/fogellmcmuffin/git_repos/irpd_ai/')
@@ -22,6 +22,9 @@ importlib.reload(f)
 ## Merging raw summary data ##
 
 def MergeRawData(summary_type: str, RA: str):
+    '''
+    Function that merges the treatments for summary data
+    '''
     if RA != 'both':
         ra_noise_raw = pd.read_csv(f'raw/{summary_type}_noise_{RA}.csv')
         ra_no_noise_raw = pd.read_csv(f'raw/{summary_type}_no_noise_{RA}.csv')
@@ -34,50 +37,40 @@ def MergeRawData(summary_type: str, RA: str):
         ra_no_noise_raw = pd.merge(ra1_no_noise_raw, ra2_no_noise_raw, 'outer')
 
     # Merging
+    ra_no_noise_raw['treatment'] = 0
+    ra_noise_raw['treatment'] = 1
     merged_raw = pd.concat([ra_no_noise_raw, ra_noise_raw], ignore_index=True, sort=False)
 
     # Export merged raw data
-    merged_raw.to_csv(f'raw_data/{summary_type}_merged_{RA}.csv', index=False)
+    merged_raw.to_csv(f'raw/{summary_type}_merged_{RA}.csv', index=False)
 
 
 ## Trimming data ##
 
 def TrimData(summary_type: str, RA: str):
     '''
-    Function that trims treatment specific summary data
+    Function that trims ra summary data
     '''
     ra_noise_trim = pd.read_csv(f'raw/{summary_type}_noise_{RA}.csv')
     ra_no_noise_trim = pd.read_csv(f'raw/{summary_type}_no_noise_{RA}.csv')
+    ra_merged_trim = pd.read_csv(f'raw/{summary_type}_merged_{RA}.csv')
     
     if summary_type == 'first' or summary_type == 'switch':
-        keep_columns = ['summary_1', 'summary_2', 'window_number', 'cooperate']
+        keep_columns = ['summary_1', 'summary_2', 'window_number', 'cooperate', 'treatment']
     else:
-        keep_columns = ['summary_1', 'summary_2', 'window_number', 'unilateral_cooperate', 'unilateral_defect']
+        keep_columns = ['summary_1', 'summary_2', 'window_number', 'unilateral_cooperate', 'unilateral_defect', 'treatment']
     
     ra_noise_trim = ra_noise_trim[ra_noise_trim.intersection(keep_columns)]
     ra_no_noise_trim = ra_no_noise_trim[ra_no_noise_trim.intersection(keep_columns)]
+    ra_merged_trim = ra_merged_trim[ra_merged_trim.intersection(keep_columns)]
     
-    return ra_noise_trim, ra_no_noise_trim
-
-
-# Creating merge df for RA summaries
-ra_no_noise_trim['treatment'] = 1
-ra_noise_trim['treatment'] = 0
-ra_merged_trim = pd.concat([ra_no_noise_trim, ra_noise_trim], ignore_index=True, sort=False)
-
-# Removing commas from summaries
-ra_noise_trim = f.remove_summary_commas(ra_noise_trim)
-ra_no_noise_trim = f.remove_summary_commas(ra_no_noise_trim)
-ra_merged_trim = f.remove_summary_commas(ra_merged_trim)
-
-# Removing 'treatment' var from ind. treatment dfs
-ra_no_noise_trim = ra_no_noise_trim.drop(['treatment'], axis=1)
-ra_noise_trim = ra_noise_trim.drop(['treatment'], axis=1)
-
-# Export trim data
-ra_no_noise_trim.to_csv(f'trim_data/RAsum_no_noise_v{version}.csv', index=False)
-ra_noise_trim.to_csv(f'trim_data/RAsum_noise_v{version}.csv', index=False)
-ra_merged_trim.to_csv(f'trim_data/RAsum_merged_v{version}.csv', index=False)
+    ra_noise_trim = f.remove_summary_commas(ra_noise_trim)
+    ra_no_noise_trim = f.remove_summary_commas(ra_no_noise_trim)
+    ra_merged_trim = f.remove_summary_commas(ra_merged_trim)
+    
+    ra_no_noise_trim.to_csv(f'trim/{summary_type}_no_noise_{RA}.csv', index=False)
+    ra_noise_trim.to_csv(f'trim/{summary_type}_noise_{RA}.csv', index=False)
+    ra_merged_trim.to_csv(f'trim/{summary_type}_merged_{RA}.csv', index=False)
 
 
 ## Creating test dataframes ##
